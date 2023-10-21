a9496164cadd1d9cb568a43d48ac6e8f7d2f40c1
Nguyen Thanh Lam <thanhlam02.haui.nd@gmail.com>
2023-10-21 13:55:24 +0700

    QLBH API

---
+++ QuanLyBanHang/QLBH.DAL/Models/CurrentProductList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace QLBH.DAL.Models
+{
+    public partial class CurrentProductList
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
---
+++ QuanLyBanHang/QLBH.DAL/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace QLBH.DAL.Models
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
